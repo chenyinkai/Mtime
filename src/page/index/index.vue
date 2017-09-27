105aa995269919410da825ffa8cca3a149d92f69
chenyinkai <287696322@qq.com>
2017-09-27 16:35:47 +0800

    add common.js; change formatTime function

--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -81,19 +81,20 @@
 		},
 		methods:{
 			time(inputTime){
-		    var date = new Date(inputTime);
-		    var y = date.getFullYear();
-		    var m = date.getMonth() + 1;
-		    m = m < 10 ? ('0' + m) : m;
-		    var d = date.getDate();
-		    d = d < 10 ? ('0' + d) : d;
-		    var h = date.getHours();
-		    h = h < 10 ? ('0' + h) : h;
-		    var minute = date.getMinutes();
-		    var second = date.getSeconds();
-		    minute = minute < 10 ? ('0' + minute) : minute;
-		    second = second < 10 ? ('0' + second) : second;
-		    return y + '-' + m + '-' + d+' '+h+':'+minute+':'+second;
+				var time = inputTime * 1000;
+			  var date = new Date(time);
+			  var y = date.getFullYear();
+			  var m = date.getMonth() + 1;
+			  m = m < 10 ? ('0' + m) : m;
+			  var d = date.getDate();
+			  d = d < 10 ? ('0' + d) : d;
+			  var h = date.getHours();
+			  h = h < 10 ? ('0' + h) : h;
+			  var minute = date.getMinutes();
+			  var second = date.getSeconds();
+			  minute = minute < 10 ? ('0' + minute) : minute;
+			  second = second < 10 ? ('0' + second) : second;
+			  return y + '-' + m + '-' + d +' '+ h +':'+ minute +':'+ second;
 			}
 		}
 	}
